ed75c028df917cba094974fd2fb118169ad9a0f0
George Ogata <george.ogata@gmail.com>
2010-06-02 18:34:40 -0400

    Bring Windows Live consumer closer into line with the others.
    Also support as elegantly as possible Windows Live's restriction on
    the domain of the URL it can redirect to. This will be further
    documented in the README.
    
    Still need to support the standard set of options to #contacts.

--- lib/contacts/windows_live.rb
+++ lib/contacts/windows_live.rb
@@ -7,130 +7,106 @@ require 'uri'
 require 'yaml'
 
 module Contacts
-  # = How I can fetch Windows Live Contacts?
-  # To gain access to a Windows Live user's data in the Live Contacts service, 
-  # a third-party developer first must ask the owner for permission. You must
-  # do that through Windows Live Delegated Authentication.
-  # 
-  # This library give you access to Windows Live Delegated Authentication System
-  # and Windows Live Contacts API. Just follow the steps below and be happy!
-  # 
-  # === Registering your app
-  # First of all, follow the steps in this 
-  # page[http://msdn.microsoft.com/en-us/library/cc287659.aspx] to register your
-  # app.
-  # 
-  # === Configuring your Windows Live YAML
-  # After registering your app, you will have an *appid*, a <b>secret key</b> and
-  # a <b>return URL</b>. Use their values to fill in the config/contacts.yml file.
-  # The policy URL field inside the YAML config file must contain the URL 
-  # of the privacy policy of your Web site for Delegated Authentication.
-  # 
-  # === Authenticating your user and fetching his contacts
-  # 
-  #   wl = Contacts::WindowsLive.new
-  #   auth_url = wl.get_authentication_url
-  # 
-  # Use that *auth_url* to redirect your user to Windows Live. He will authenticate
-  # there and Windows Live will POST to your return URL. You have to get the
-  # body of that POST, let's call it post_body. (if you're using Rails, you can 
-  # get the POST body through request.raw_post, in the context of an action inside
-  # ActionController)
-  #
-  # Now, to fetch his contacts, just do this:
-  #
-  #   contacts = wl.contacts(post_body)
-  #   #-> [ ['Fitzgerald', 'fubar@gmail.com', 'fubar@example.com'],
-  #         ['William Paginate', 'will.paginate@gmail.com'], ...
-  #       ]
-  #--
-  # This class has two responsibilities:
-  # 1. Access the Windows Live Contacts API through Delegated Authentication
-  # 2. Import contacts from Windows Live and deliver it inside an Array
-  #
-  class WindowsLive
-    CONFIG_FILE = File.dirname(__FILE__) + '/../config/contacts.yml'
+  class WindowsLive < Consumer
+    configuration_attribute :application_id
+    configuration_attribute :secret_key
+    configuration_attribute :security_algorithm
+    configuration_attribute :privacy_policy_url
+    configuration_attribute :return_url
 
-    # Initialize a new WindowsLive object.
-    #    
-    # ==== Paramaters
-    # * config_file <String>:: The contacts YAML config file name
-    #--
-    # You can check an example of a config file inside config/ directory
     #
-    def initialize(config_file=CONFIG_FILE)
-      confs = YAML.load_file(config_file)['windows_live']
-      @wll = WindowsLiveLogin.new(confs['appid'], confs['secret'], confs['security_algorithm'],
-                                  nil, confs['policy_url'], confs['return_url'])
+    # If this is set, then #authentication_url will force the given
+    # +target+ URL to have this origin (= scheme + host + port). This
+    # should match the domain that your Windows Live project is
+    # configured to live on.
+    #
+    # Instead of calling #authorize(params) when the user returns, you
+    # will need to call #forced_redirect_url(params) to redirect the
+    # user to the true contacts handler. #forced_redirect_url will
+    # handle construction of the query string based on the incoming
+    # parameters.
+    #
+    # The intended use is for development mode on localhost, which
+    # Windows Live forbids redirection to. Instead, you may register
+    # your app to live on "http://myapp.local", set :force_origin =>
+    # 'http://myapp.local:3000', and map the domain to 127.0.0.1 via
+    # your local hosts file. Your handlers will then look something
+    # like:
+    #
+    #     def handler
+    #       if ENV['HTTP_METHOD'] == 'POST'
+    #         consumer = Contacts::WindowsLive.new
+    #         redirect_to consumer.authentication_url(session)
+    #       else
+    #         consumer = Contacts::WindowsLive.deserialize(session[:consumer])
+    #         consumer.authorize(params)
+    #         contacts = consumer.contacts
+    #       end
+    #     end
+    #
+    # Since only the origin is forced -- not the path part of the URL
+    # -- the handler typically redirects to itself. The second time
+    # through it is a GET request.
+    #
+    # Default: nil
+    #
+    # Example: http://myapp.local
+    #
+    configuration_attribute :force_origin
+
+    def initialize(options={})
+      @wll = WindowsLiveLogin.new(application_id, secret_key, security_algorithm, nil, privacy_policy_url, return_url)
+      @consent = {}
     end
 
+    def initialize_serialized(data)
+      params = query_to_params(data)
+      @consent = params['consent']
+    end
 
-    # Windows Live Contacts API need to authenticate the user that is giving you
-    # access to his contacts. To do that, you must give him a URL. That method
-    # generates that URL. The user must access that URL, and after he has done
-    # authentication, hi will be redirected to your application.
-    #
-    def get_authentication_url
-      @wll.getConsentUrl("Contacts.Invite")
+    def serialize
+      params_to_query('consent' => @consent)
     end
-    
-    # After the user has been authenticaded, Windows Live Delegated Authencation
-    # Service redirects to your application, through a POST HTTP method. Along
-    # with the POST, Windows Live send to you a Consent that you must process
-    # to access the user's contacts. This method process the Consent 
-    # to you.
-    #
-    # ==== Paramaters
-    # * consent <String>:: A string containing the Consent given to you inside
-    # the redirection POST from Windows Live
-    #
-    def process_consent(consent)
-      consent.strip!
-      consent = URI.unescape(consent)
-      @consent_token = @wll.processConsent(consent)
+
+    def authentication_url(target, options={})
+      if force_origin
+        context = target
+        target = force_origin + URI.parse(target).path
+      end
+      @wll.getConsentUrl("Contacts.Invite", context, target)
     end
-    
-    # This method return the user's contacts inside an Array in the following
-    # format:
-    #
-    #   [ 
-    #     ['Brad Fitzgerald', 'fubar@gmail.com'],
-    #     [nil, 'nagios@hotmail.com'],
-    #     ['William Paginate', 'will.paginate@yahoo.com']  ...
-    #   ]
-    #
-    # ==== Paramaters
-    # * consent <String>:: A string containing the Consent given to you inside 
-    # the redirection POST from Windows Live
-    #
-    def contacts(consent)
-      process_consent(consent)
-      contacts_xml = access_live_contacts_api()
+
+    def forced_redirect_url(params)
+      target_origin = params['appctx'] and
+        "#{target_origin}?#{params_to_query(params)}"
+    end
+
+    def authorize(params)
+      @consent = params['ConsentToken']
+    end
+
+    def contacts(options={})
+      # TODO: support standard Contacts options.
+      consent_token = @wll.processConsentToken(@consent, nil)
+      contacts_xml = access_live_contacts_api(consent_token)
       contacts_list = WindowsLive.parse_xml(contacts_xml)
     end
 
-    # This method access the Windows Live Contacts API Web Service to get
-    # the XML contacts document
-    #
-    def access_live_contacts_api
+    private
+
+    def access_live_contacts_api(consent_token)
       http = http = Net::HTTP.new('livecontacts.services.live.com', 443)
       http.use_ssl = true
 
       response = nil
       http.start do |http|
-         request = Net::HTTP::Get.new("/users/@L@#{@consent_token.locationid}/rest/invitationsbyemail", {"Authorization" => "DelegatedToken dt=\"#{@consent_token.delegationtoken}\""})
+         request = Net::HTTP::Get.new("/users/@L@#{consent_token.locationid}/rest/invitationsbyemail", {"Authorization" => "DelegatedToken dt=\"#{consent_token.delegationtoken}\""})
          response = http.request(request)
       end
 
       return response.body
     end
-    
-    # This method parses the XML Contacts document and returns the contacts
-    # inside an Array
-    #
-    # ==== Paramaters
-    # * xml <String>:: A string containing the XML contacts document
-    #
+
     def self.parse_xml(xml)
       doc = Hpricot::XML(xml)
 
@@ -160,5 +136,4 @@ module Contacts
       return contacts
     end
   end
-
 end
