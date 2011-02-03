463eccca08f527ed450408980a1005edbcdd0dd0
Wil Chung <iamwil@gmail.com>
2011-02-03 10:16:42 -0800

    allow the projection to be changed from the options
    fix bug: projection is not named thickness.

--- lib/contacts/google.rb
+++ lib/contacts/google.rb
@@ -17,13 +17,23 @@ module Contacts
       super(CONSUMER_OPTIONS, REQUEST_TOKEN_PARAMS)
     end
 
+    # retrieve the contacts for the user's account
+    #
+    # The options it takes are:
+    #
+    #   - limit - the max number of results to return
+    #     ("max-results"). Defaults to 200
+    #   - offset - the index to start returning results for pagination ("start-index")
+    #   - projection - defaults to thin
+    #       http://code.google.com/apis/contacts/docs/3.0/reference.html#Projections
     def contacts(options={})
       return nil if @access_token.nil?
       params = {:limit => 200}.update(options)
       google_params = translate_parameters(params)
       query = params_to_query(google_params)
+      projection = options[:projection] || "thin"
       begin
-        response = @access_token.get("http://www.google.com/m8/feeds/contacts/default/thin?#{query}")
+        response = @access_token.get("http://www.google.com/m8/feeds/contacts/default/#{projection}?#{query}")
       rescue OAuth::Unauthorized => error
         # Token probably expired.
         @error = error.message
